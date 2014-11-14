855c41764b73743b581e4c02ac9c568bb1883d32
aldy120 <aldy12345@gmail.com>
2014-11-15 02:36:21 +0800

    update hw2

---
+++ hw2.R
@@ -0,0 +1,23 @@
+#theFunction(x)=1-exp(-x)
+par(mfrow = c(1,2))
+
+InverseOfTheFunction=function(x)# inverse funtion
+{
+  y=-log(1-x)
+  return(y)
+}
+
+DerivativeOfTheFunction=function(x)
+{
+  y=exp(-x)# derivative
+  return(y)
+}
+
+x1=runif(10000)# 0~1
+y1=InverseOfTheFunction(x1)
+hist(y1,breaks=50)
+
+x2=seq(0.05,8,0.01)
+y2=DerivativeOfTheFunction(x2)
+plot(x2,y2,"l")
+
